8ab026e7c73f2e75f53952b10df2f1357e5236f7
miguelkapicius <miguelkapicius@gmail.com>
2024-03-26 13:47:25 -0300

    Aula 02 Finalizada

--- script.js
+++ script.js
@@ -2,22 +2,52 @@ const html = document.querySelector("html");
 const focoBtn = document.querySelector(".app__card-button--foco");
 const curtoBtn = document.querySelector(".app__card-button--curto");
 const longoBtn = document.querySelector(".app__card-button--longo");
-const timer = document.querySelector("#timer");
+let timer = document.querySelector("#timer");
 const imagem = document.querySelector(".app__image");
 const texto = document.querySelector(".app__title");
-const play = document.querySelector("#start-pause")
+const play = document.querySelector("#start-pause");
+
+function alterarContexto(contexto) {
+  html.setAttribute("data-contexto", `${contexto}`);
+  imagem.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      texto.innerHTML = `
+      Otimize sua produtividade,<br>
+      <strong class="app__title-strong">mergulhe no que importa.</strong>
+      `;
+      timer = 1500;
+      break;
+
+    case "descanso-curto":
+      texto.innerHTML = `
+      Que tal dar uma respirada?<br>
+      <strong class="app__title-strong">Faça uma pausa curta!</strong>
+      `;
+      timer = 300;
+      break;
+
+    case "descanso-longo":
+      texto.innerHTML = `
+      Hora de voltar à superfície.<br>
+      <strong class="app__title-strong">Faça uma pausa longa.</strong>
+      `;
+      timer = 900;
+      break;
+
+    default:
+      break;
+  }
+}
 
 focoBtn.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "foco");
-  timer = 1500;
+  alterarContexto("foco");
 });
 
 curtoBtn.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-curto");
-  timer = 300;
+  alterarContexto("descanso-curto");
 });
 
 longoBtn.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-longo");
-  timer = 900;
+  alterarContexto("descanso-longo");
 });
